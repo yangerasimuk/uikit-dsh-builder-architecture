404e0cc498be6f6528ef07383f450e0113a2922e
Yan Gerasimuk <yan.gerasimuk@gmail.com>
2024-06-18 16:57:59 +0300

    Init XCode project

---
+++ DSHBuilder/DSHBuilder/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DSHBuilder
+//
+//  Created by Yan Gerasimuk on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
